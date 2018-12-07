4ed5e4ef401364a10451bcbdac3435f067736345
mailtsjp <mailtsjp@gmail.com>
2018-12-07 13:15:43 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,39 @@
+# 微信自动回复功能
+
+## 如何使用？
+ 1.安装python2.7  
+ 
+ 2.安装python pip
+ 
+ 3.安装相关模块  
+```
+$ pip install itchat
+$ pip install requests
+$ pip install time
+$ pip install re
+$ pip install sys
+$ pip install urllib2
+$ pip install urllib
+$ pip install json
+```
+4.运行模块  
+```
+python auto-reply-wechat.py
+```
+
+5.运行成功后，电脑会弹出二维码，手机扫描登录微信  
+![](2.jpg)
+
+6.登录成功后，最终的回复效果  
+
+![](3.jpg)
+
+## 没有实现的功能？
+1.图片的自动回复
+
+2.指定人的自动回复
+
+![](1.jpg)
+
+## 作品参考
+https://gist.github.com/yfgeek/d38775aac03f3b2d7371d2bea17766d0
---
+++ auto-reply-wechat.py
@@ -0,0 +1,38 @@
+﻿#!/usr/bin/env python
+# -- coding: utf-8 --
+import itchat, time, re, sys, urllib2, json
+from urllib import quote,unquote,urlencode
+from itchat.content import *
+@itchat.msg_register([TEXT])
+def text_reply(msg):
+    global index
+    index = index + 1
+    if index == 1:
+        m1 = unicode('我已经休息或者在上课,下面的内容是我的语言机器人自动回复的','gb2312')
+        itchat.send((m1),msg['FromUserName'])
+        m2 = unicode('对了,它比较粗鲁,请不要轻易说脏话','gb2312')
+        itchat.send((m2),msg['FromUserName'])    
+    else:
+	    m = quote(msg['Text'].encode('utf8'))
+	    #http://api.qingyunke.com/api.php?key=free&appid=0&msg=  
+	    #url = 'http://sandbox.api.simsimi.com/request.p?key=9470057a-909b-4e6c-a25d-19e0834d6667&lc=zh&ft=1.0&text='+m
+	    url = 'http://api.qingyunke.com/api.php?key=free&appid=0&msg='+m
+	    print url
+	    raw = urllib2.urlopen(url)
+	    try:
+	      data = json.loads(raw.read().decode('utf-8'))
+	      #print data['response']
+	      print data['content']
+	      itchat.send((data['content']), msg['FromUserName'])
+	    except:
+	        pass	  
+@itchat.msg_register([PICTURE, RECORDING, VIDEO, SHARING])
+def pic_reply(msg):
+    itchat.send((unicode('图片不错','gb2312')),msg['FromUserName'])
+    #itchat.send('@img@1.jpg', msg['FromUserName'])
+if __name__ == '__main__':
+    default_encoding = 'utf-8'
+    index = 0
+    if sys.getdefaultencoding() != default_encoding:
+        itchat.auto_login(enableCmdQR=False,hotReload=True)
+        itchat.run()
